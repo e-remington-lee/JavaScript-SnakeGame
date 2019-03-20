5ac1eca26a0ebe72c2640a4d286cf7e7f9d44b17
Erik Remington Lee <eriklee19@gmail.com>
2019-03-19 21:45:51 -0400

    3/19/19 edits

--- snake.js
+++ snake.js
@@ -1,20 +1,74 @@
 //Snake game app
-var canvas = document.getElementById('snakeGame');
-var context = canvas.getContext('2d')
-var snakeL = 10
-var snakeW = 20
+
+var canvas;
+var context;
+
+var snakeL = 20
+var snakeW = 10
+var x = 200
+var y = 100
+var yspeed = 5
+
+// var canvas = document.getElementById('snakeGame');
+// var context = canvas.getContext('2d');
 
 
-//canvas
-draw(0,0,800,600, 'black');
+window.onload = function (){
+    canvas = document.getElementById('snakeGame');
+    context = canvas.getContext('2d');
+    canvas.setAttribute('tabindex', 0);
+    var fps = 60;
+    setInterval(function() {
+        drawAll();
+        // move();
+    }
+        ,1000/fps);
 
-//snake
-draw(100,200, 10,15, 'blue');
+    canvas.addEventListener('keydown', function(ev) {
+        var keyReturn = moveSnake(ev);
+        console.log(keyReturn);
+    });
+        
+ 
 
-//apple
-draw(300,300,10,10, 'red');
-
-function draw(leftX, topY, width, height, drawColor) {
-    context.fillStyle = drawColor;
-    context.fillRect(leftX, topY, width, height);
 }
+
+
+function moveSnake(ev){
+    canvas.getBoundingClientRect();
+    document.documentElement;
+    var code = ev.keyCode;
+    return{code};
+    
+}
+
+
+function drawAll () {
+    //canvas
+    draw(0,0,800,600, 'black');
+
+    //snake
+    draw(x,y, snakeW,snakeL, 'blue');
+
+    //apple
+    draw(300,300,10,10, 'red');
+
+    function draw(leftX, topY, width, height, drawColor) {
+        context.fillStyle = drawColor;
+        context.fillRect(leftX, topY, width, height);
+    }
+
+
+  
+}
+
+// function move(){
+//     y += yspeed;
+//     if(y > canvas.height) {
+//     yspeed = -yspeed;
+//     }
+
+//         if(y < 0) {
+//         yspeed = -yspeed;
+//         }
+// }
